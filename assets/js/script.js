8b4ee9d6b7fadfc942a08430beb6c34039655431
rafliogun49 <67999361+rafliogun49@users.noreply.github.com>
2021-05-13 16:28:42 +0700

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-// hamburger
-const hamBurger = document.querySelector(".ham-burger");
-
-hamBurger.addEventListener("click", function() {
-    document.querySelector(".nav").classList.toggle("show")
-})
